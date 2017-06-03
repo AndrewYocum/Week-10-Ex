0e0773421ed6f669beaee9c13bbb149a0fbc1ca7
Andrew Yocum <sagyocum@andrew-pro.local>
2017-06-03 10:26:42 -0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var colors = [
+  "red", "blue", "purple", "pink", "yellow"
+]
+var tat = colors.length
+console.log(tat)
+
+console.log(
+  colors.reverse()
+)
+
+var numbers = [
+  1, 2, 3, 4, 5, 6, 7, 8
+]
+
+console.log(
+  numbers.sort()
+)
+
+console.log(
+  numbers.reverse()
+)
+
+var example = [
+  "string1", "string2", "string3"
+]
+
+example.push("string4", "string5")
+
+console.log(
+  example
+)
+
+example.splice(0, 1)
+console.log(example)
